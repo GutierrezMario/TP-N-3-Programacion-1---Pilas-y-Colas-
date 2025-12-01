e64d8b1d84d534ad5b38e049e87e47327768c88c
GutierrezMario <mariogtz745@gmail.com>
2025-12-01 19:24:51 -0300

    Profesor.java

---
+++ TP N°3 Pilas y Colas - Programacion 1/Profesor.java
@@ -0,0 +1,7 @@
+public class Profesor {
+    private String nombre;
+    private Cola colaConsultas;
+
+    public Profesor(String nombre) {
+        this.nombre = nombre;
+        this.colaConsultas = new Cola
